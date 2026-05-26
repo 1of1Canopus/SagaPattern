0f96c453d04ccf41ad6f31ec5896954d4cadaf78
Canopus <souhaile.safouani@gmail.com>
2026-05-26 15:13:01 +0200

    Fix KafkaTemplate injection: explicit ProducerFactory<String,Object> and KafkaTemplate<String,Object> beans

--- src/test/java/io/housedevinci/sagapattern/config/AxonTestConfig.java
+++ src/test/java/io/housedevinci/sagapattern/config/AxonTestConfig.java
@@ -1,11 +1,5 @@
 package io.housedevinci.sagapattern.config;
 
-import org.axonframework.commandhandling.CommandBus;
-import org.axonframework.commandhandling.SimpleCommandBus;
-import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.axonframework.commandhandling.gateway.DefaultCommandGateway;
-import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
-import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -19,23 +13,5 @@ public class AxonTestConfig {
     @Primary
     public EventStorageEngine eventStorageEngine() {
         return new InMemoryEventStorageEngine();
-    }
-
-    @Bean
-    @Primary
-    public EventStore eventStore(EventStorageEngine engine) {
-        return EmbeddedEventStore.builder().storageEngine(engine).build();
-    }
-
-    @Bean
-    @Primary
-    public CommandBus commandBus() {
-        return SimpleCommandBus.builder().build();
-    }
-
-    @Bean
-    @Primary
-    public CommandGateway commandGateway(CommandBus commandBus) {
-        return DefaultCommandGateway.builder().commandBus(commandBus).build();
     }
 }
